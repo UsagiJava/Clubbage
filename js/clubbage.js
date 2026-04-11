933a5c63328232301682ddec739e4773931ee462
Grrrraffe <grrrraffe@outlook.com>
2026-04-10 17:59:20 -0700

    04/10/2026: start of clubbage game repository with some notes.

---
+++ js/clubbage.js
@@ -0,0 +1,56 @@
+
+
+/*
+1. Click a card. Lowest card gets the first crib and becomes the dealer. If tie, initial dealer gets the crib.
+
+2. Deal 6 cards to each player. Each player discards 2 cards to the crib. The dealer gets the crib. (ring intro)
+
+3. Top card of the deck is turned up. This is the starter card. (bell rings - round starts)
+
+4. Flurry portion of game starts.
+Starting with the non-dealer, each player takes turns playing one card at a time, announcing the cumulative total of the cards played. The total cannot exceed 31.
+If a player cannot play a card without exceeding 31, they say "Go" and the other player continues to play until they also cannot play or reach 31.
+The player who plays the last card scores 1 point for "Go" or 2 points for reaching 31.
+Players alternate playing cards until all cards have been played.
+    Ways to score points:
+    a. hit 15 for 2 points.
+    b. last card played (called "Go") for 1 point, or 2 points if the total is exactly 31.
+Note: if a player gets 3 or more points at one time, the opponent is knocked down.  3 knockdowns per round = TKO.
+Note: jab animation for hitting 1-2 points, hook animation for hitting 3-4 points, uppercut animation for hitting 5 or more points.
+
+5. Haymakers portion of game starts. Players score their hands and the crib (haymaker follow-up). The non-dealer scores first, then the dealer scores.
+    Ways to score points:
+    a. nobs for 1 point if the player has a Jack of the same suit as the starter card.
+    b. each sum of 15 worth 2 points.
+    c. pairs (2 points for a pair, 6 points for three of a kind, 12 points for four of a kind)
+    d. runs of three or more cards (3 points for a run of three, 4 points for a run of four, etc.)
+    e. having a flush of four or more cards (4 points for a flush of four, 5 points for a flush of five including the starter card)
+    f. having a Jack of the same suit as the starter card (called "his nobs", 1 point)
+Note: no kockdown during haymakers, but if a player loses all their hitpoints during haymakers, they are knocked out. End of match.
+Note: jab animation for hitting 1-2 points, hook animation for hitting 3-4 points, uppercut animation for hitting 5 or more points.
+
+6. Bell rings, players go back to their corners.
+
+7. Deal 6 cards to each player. The the non-dealer becomes the dealer. Each player discards 2 cards to the crib. maybe trainer in corner hand moves fast for a "hint" to which 2 cards to drop?
+
+8. goto step 3.
+
+
+
+Each match lasts until hitpoints are depleted or if a player is knocked down three times in a round.
+At the end of a match, show the stats:
+Total damage dealt
+Total damage done during flurry
+Total damage done during haymaker
+Total damage done during haymaker follow-up (crib)
+
+
+A training montage cutscene plays between bouts.  This increases the hitpoints for a user for next match.
+
+The game would have three main opponents to beat to get the belt.
+
+First match: Delusional Danthony (both start with 20 HP)
+Second match: <your character> (both start with 40 HP)
+Title match: Andre the Uncompliant (both start with 60 HP)
+
+*/
